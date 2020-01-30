08ace99a7134feef6d383bc15c76753e2fdee1a5
linux <gan.gaving@gmail.com>
2020-01-30 09:01:32 -0500

    Objective 3: enhance GET /people wit sorting

--- connections/views.py
+++ connections/views.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 from flask import Blueprint
+from webargs import fields
 from webargs.flaskparser import use_args
 
 from connections.models.person import Person
@@ -10,9 +11,26 @@ blueprint = Blueprint('connections', __name__)
 
 
 @blueprint.route('/people', methods=['GET'])
-def get_people():
+@use_args({'sort': fields.Str(location='query')})
+def get_people(args):
     people_schema = PersonSchema(many=True)
-    people = Person.query.all()
+
+    order_query = None
+    if(args):
+        if(args['sort'] == "-created_at"):
+            order_query = Person.created_at.desc()
+        elif(args['sort'] == "created_at"):
+            order_query = Person.created_at
+        elif(args['sort'] == "-first_name"):
+            order_query = Person.first_name.desc()
+        elif(args['sort'] == "first_name"):
+            order_query = Person.first_name
+        elif(args['sort'] == "-last_name"):
+            order_query = Person.last_name.desc()
+        elif(args['sort'] == "last_name"):
+            order_query = Person.last_name
+
+    people = Person.query.order_by(order_query).all()
     return people_schema.jsonify(people), HTTPStatus.OK
 
 
